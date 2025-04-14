c9dc05baa9db0eb0e57d56cbcf76a703eaf1983e
krseo71 <tjrudfhr123@naver.com>
2025-04-14 21:03:31 +0900

    [FIX] 2. 변수의 이름을 더 명확하게 변경 thisAmount -> result, perf -> aPerformance

--- src/main/java/com/tkl/refactoring/chapter1/StatementPrinterV1.java
+++ src/main/java/com/tkl/refactoring/chapter1/StatementPrinterV1.java
@@ -34,26 +34,27 @@ public class StatementPrinterV1 {
     }
 
     //  1. switch문 함수 추출하기
-    private static int amountFor(Performance perf, Play play) {
-        int thisAmount;
+    private static int amountFor(Performance aPerformance, Play play) {
+        // 2. 변수의 이름을 더 명확하게 변경 thisAmount -> result
+        int result;
         switch (play.type()) {
             case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience() > 30) {
-                    thisAmount += 1000 * (perf.audience() - 30);
+                result = 40000;
+                if (aPerformance.audience() > 30) {
+                    result += 1000 * (aPerformance.audience() - 30);
                 }
                 break;
             case "comedy":
-                thisAmount = 30000;
-                if (perf.audience() > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience() - 20);
+                result = 30000;
+                if (aPerformance.audience() > 20) {
+                    result += 10000 + 500 * (aPerformance.audience() - 20);
                 }
-                thisAmount += 300 * perf.audience();
+                result += 300 * aPerformance.audience();
                 break;
             default:
                 throw new Error("unknown type: ${play.type}");
         }
-        return thisAmount;
+        return result;
     }
 
 }
