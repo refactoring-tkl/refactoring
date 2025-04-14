3594fc2391eb0e121a20062e2c77f9fda99c9ca2
krseo71 <tjrudfhr123@naver.com>
2025-04-14 21:00:06 +0900

    [FIX] 1. 함수 추출하기

---
+++ src/main/java/com/tkl/refactoring/chapter1/StatementPrinterV1.java
@@ -0,0 +1,59 @@
+package com.tkl.refactoring.chapter1;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Map;
+
+public class StatementPrinterV1 {
+
+    public String print(Invoice invoice, Map<String, Play> plays) {
+        int totalAmount = 0;
+        int volumeCredits = 0;
+        String result = String.format("Statement for %s\n", invoice.customer());
+
+        NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
+
+        for (Performance perf : invoice.performances()) {
+            Play play = plays.get(perf.playID());
+            int thisAmount;
+
+            thisAmount = amountFor(perf, play);
+
+            // add volume credits
+            volumeCredits += Math.max(perf.audience() - 30, 0);
+            // add extra credit for every ten comedy attendees
+            if ("comedy".equals(play.type())) volumeCredits += perf.audience() / 5;
+
+            // print line for this order
+            result += String.format("  %s: %s (%s seats)\n", play.name(), frmt.format(thisAmount / 100), perf.audience());
+            totalAmount += thisAmount;
+        }
+        result += String.format("Amount owed is %s\n", frmt.format(totalAmount / 100));
+        result += String.format("You earned %s credits\n", volumeCredits);
+        return result;
+    }
+
+    //  1. switch문 함수 추출하기
+    private static int amountFor(Performance perf, Play play) {
+        int thisAmount;
+        switch (play.type()) {
+            case "tragedy":
+                thisAmount = 40000;
+                if (perf.audience() > 30) {
+                    thisAmount += 1000 * (perf.audience() - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (perf.audience() > 20) {
+                    thisAmount += 10000 + 500 * (perf.audience() - 20);
+                }
+                thisAmount += 300 * perf.audience();
+                break;
+            default:
+                throw new Error("unknown type: ${play.type}");
+        }
+        return thisAmount;
+    }
+
+}
