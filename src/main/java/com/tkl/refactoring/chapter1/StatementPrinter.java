6d5c961f9926c3f62a5710c153c01e03f93016a1
jeonghun3.kim <jeonghun3.kim@bugs.co.kr>
2025-04-15 08:41:11 +0900

    [REFACTOR] 변수명변경

--- src/main/java/com/tkl/refactoring/chapter1/StatementPrinter.java
+++ src/main/java/com/tkl/refactoring/chapter1/StatementPrinter.java
@@ -33,25 +33,25 @@ public class StatementPrinter {
     }
 
     private int amountFor(Performance perf, Play play) {
-        int thisAmount;
+        int result;
         switch (play.type()) {
             case "tragedy":
-                thisAmount = 40000;
+                result = 40000;
                 if (perf.audience() > 30) {
-                    thisAmount += 1000 * (perf.audience() - 30);
+                    result += 1000 * (perf.audience() - 30);
                 }
                 break;
             case "comedy":
-                thisAmount = 30000;
+                result = 30000;
                 if (perf.audience() > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience() - 20);
+                    result += 10000 + 500 * (perf.audience() - 20);
                 }
-                thisAmount += 300 * perf.audience();
+                result += 300 * perf.audience();
                 break;
             default:
                 throw new Error("unknown type: ${play.type}");
         }
-        return thisAmount;
+        return result;
     }
 
 }
