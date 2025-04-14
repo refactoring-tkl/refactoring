00e1ad5cc960cb2b3324ff1d2751268d6fb63b02
jeonghun3.kim <jeonghun3.kim@bugs.co.kr>
2025-04-15 08:40:15 +0900

    [REFACTOR] 메서드추출 - switch문 메서드로 추출

--- src/main/java/com/tkl/refactoring/chapter1/StatementPrinter.java
+++ src/main/java/com/tkl/refactoring/chapter1/StatementPrinter.java
@@ -15,25 +15,8 @@ public class StatementPrinter {
 
         for (Performance perf : invoice.performances()) {
             Play play = plays.get(perf.playID());
-            int thisAmount;
 
-            switch (play.type()) {
-                case "tragedy":
-                    thisAmount = 40000;
-                    if (perf.audience() > 30) {
-                        thisAmount += 1000 * (perf.audience() - 30);
-                    }
-                    break;
-                case "comedy":
-                    thisAmount = 30000;
-                    if (perf.audience() > 20) {
-                        thisAmount += 10000 + 500 * (perf.audience() - 20);
-                    }
-                    thisAmount += 300 * perf.audience();
-                    break;
-                default:
-                    throw new Error("unknown type: ${play.type}");
-            }
+            int thisAmount = amountFor(perf, play);
 
             // add volume credits
             volumeCredits += Math.max(perf.audience() - 30, 0);
@@ -47,6 +30,28 @@ public class StatementPrinter {
         result += String.format("Amount owed is %s\n", frmt.format(totalAmount / 100));
         result += String.format("You earned %s credits\n", volumeCredits);
         return result;
+    }
+
+    private int amountFor(Performance perf, Play play) {
+        int thisAmount;
+        switch (play.type()) {
+            case "tragedy":
+                thisAmount = 40000;
+                if (perf.audience() > 30) {
+                    thisAmount += 1000 * (perf.audience() - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (perf.audience() > 20) {
+                    thisAmount += 10000 + 500 * (perf.audience() - 20);
+                }
+                thisAmount += 300 * perf.audience();
+                break;
+            default:
+                throw new Error("unknown type: ${play.type}");
+        }
+        return thisAmount;
     }
 
 }
