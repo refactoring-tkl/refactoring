7e9692e44710c7b9ee5d204377ce7851e932f0ad
krseo <krseo@nhnlink.co.kr>
2025-04-16 19:45:54 +0900

    [REFACTOR] playFor 수정, static method에서 static 제거, 변수명 변경, 파라미터에 Map<String, Play> 추가

--- src/main/java/com/tkl/refactoring/chapter1/StatementPrinterV1.java
+++ src/main/java/com/tkl/refactoring/chapter1/StatementPrinterV1.java
@@ -11,64 +11,62 @@ public class StatementPrinterV1 {
         String result = String.format("Statement for %s\n", invoice.customer());
         NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
-        for (Performance perf : invoice.performances()) {
-            result += String.format("  %s: %s (%s seats)\n", playFor(perf).name(), frmt.format(amountFor(perf) / 100), perf.audience());
+        for (Performance performance : invoice.performances()) {
+            result += String.format("  %s: %s (%s seats)\n", playFor(performance, plays).name(), frmt.format(amountFor(performance, plays) / 100), performance.audience());
         }
 
-        result += String.format("Amount owed is %s\n", frmt.format(totalAmount(invoice) / 100));
-        result += String.format("You earned %s credits\n", totalVolumeCredits(invoice));
+        result += String.format("Amount owed is %s\n", frmt.format(totalAmount(invoice, plays) / 100));
+        result += String.format("You earned %s credits\n", totalVolumeCredits(invoice, plays));
         return result;
     }
 
-    private static int totalAmount(Invoice invoice) {
+    private int totalAmount(Invoice invoice, Map<String, Play> plays) {
         int result = 0;
-        for (Performance perf : invoice.performances()) {
-            result += amountFor(perf);
-        }
-        return result;
-    }
-
-    private static int totalVolumeCredits(Invoice invoice) {
-        int result = 0;
-        for (Performance perf : invoice.performances()) {
-            result += volumeCreditsFor(perf);
+        for (Performance performance : invoice.performances()) {
+            result += amountFor(performance, plays);
         }
         return result;
     }
 
-    private static int volumeCreditsFor(Performance aPerformance) {
+    private int totalVolumeCredits(Invoice invoice, Map<String, Play> plays) {
         int result = 0;
-        // add volume credits
-        result += Math.max(aPerformance.audience() - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy".equals(playFor(aPerformance).type())) result += aPerformance.audience() / 5;
+        for (Performance performance : invoice.performances()) {
+            result += volumeCreditsFor(performance, plays);
+        }
         return result;
     }
 
-    private static Play playFor(Performance perf) {
-        return ((Map<String, Play>) new HashMap<String, Play>()).get(perf.playID());
+    private int volumeCreditsFor(Performance performance, Map<String, Play> plays) {
+        int result = 0;
+        result += Math.max(performance.audience() - 30, 0);
+        if ("comedy".equals(playFor(performance, plays).type())) result += performance.audience() / 5;
+        return result;
+    }
+
+    private Play playFor(Performance performance, Map<String, Play> plays) {
+		return plays.get(performance.playID());
     }
 
     //  1. switch문 함수 추출하기
-    private static int amountFor(Performance aPerformance) {
+    private int amountFor(Performance performance, Map<String, Play> plays) {
         // 2. 변수의 이름을 더 명확하게 변경 thisAmount -> result
         int result;
-        switch (playFor(aPerformance).type()) {
+        switch (playFor(performance, plays).type()) {
             case "tragedy":
                 result = 40000;
-                if (aPerformance.audience() > 30) {
-                    result += 1000 * (aPerformance.audience() - 30);
+                if (performance.audience() > 30) {
+                    result += 1000 * (performance.audience() - 30);
                 }
                 break;
             case "comedy":
                 result = 30000;
-                if (aPerformance.audience() > 20) {
-                    result += 10000 + 500 * (aPerformance.audience() - 20);
+                if (performance.audience() > 20) {
+                    result += 10000 + 500 * (performance.audience() - 20);
                 }
-                result += 300 * aPerformance.audience();
+                result += 300 * performance.audience();
                 break;
             default:
-                throw new Error("unknown type: ${playFor(aPerformance).type()}");
+                throw new Error("unknown type: ${playFor(performance).type()}");
         }
         return result;
     }
---
+++ src/test/java/com/tkl/refactoring/chapter1/StatementPrinterV1Test.java
@@ -0,0 +1,54 @@
+package com.tkl.refactoring.chapter1;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class StatementPrinterV1Test {
+    static final String GIVEN_RESULT = """
+            Statement for BigCo
+              Hamlet: $650.00 (55 seats)
+              As You Like It: $580.00 (35 seats)
+              Othello: $500.00 (40 seats)
+            Amount owed is $1,730.00
+            You earned 47 credits
+            """;
+
+    @Test
+    void exampleStatement() {
+        Map<String, Play> plays = Map.of(
+                "hamlet", new Play("Hamlet", "tragedy"),
+                "as-like", new Play("As You Like It", "comedy"),
+                "othello", new Play("Othello", "tragedy"));
+
+        Invoice invoice = new Invoice("BigCo", List.of(
+                new Performance("hamlet", 55),
+                new Performance("as-like", 35),
+                new Performance("othello", 40)));
+
+        StatementPrinterV1 statementPrinter = new StatementPrinterV1();
+        var result = statementPrinter.print(invoice, plays);
+
+        assertThat(result).isEqualTo(GIVEN_RESULT);
+    }
+
+    @Test
+    void statementWithNewPlayTypes() {
+        Map<String, Play> plays = Map.of(
+                "henry-v", new Play("Henry V", "history"),
+                "as-like", new Play("As You Like It", "pastoral"));
+
+        Invoice invoice = new Invoice("BigCo", List.of(
+                new Performance("henry-v", 53),
+                new Performance("as-like", 55)));
+
+        StatementPrinterV1 statementPrinter = new StatementPrinterV1();
+        Assertions.assertThrows(Error.class, () -> statementPrinter.print(invoice, plays));
+    }
+
+}
