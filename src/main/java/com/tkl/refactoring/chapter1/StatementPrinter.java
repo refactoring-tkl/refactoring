4e0d96e8b82b846a0d418d7d8507555e2a5896db
jeonghun3.kim <jeonghun3.kim@bugs.co.kr>
2025-04-17 09:09:41 +0900

    [REFACTOR] 중간데이터 구조 중첩클래스 사용하도록 변경(Performance -> StatementData.StatementPerformance)

--- src/main/java/com/tkl/refactoring/chapter1/StatementPrinter.java
+++ src/main/java/com/tkl/refactoring/chapter1/StatementPrinter.java
@@ -5,106 +5,135 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class StatementPrinter {
 
-    public String print(Invoice invoice, Map<String, Play> plays) {
-        StatementData statementData = new StatementData(invoice, plays);
-        return renderPlainText(statementData);
-    }
+	public String print(Invoice invoice, Map<String, Play> plays) {
+		StatementData statementData = new StatementData(invoice, plays);
+		return renderPlainText(statementData);
+	}
 
-    private String renderPlainText(StatementData statementData) {
-        String result = String.format("Statement for %s\n", statementData.customer());
+	private String renderPlainText(StatementData statementData) {
+		String result = String.format("Statement for %s\n", statementData.customer());
 
-        for (Performance perf : statementData.performances()) {
-            Play play = statementData.plays().get(perf.playID());
+		for (StatementData.StatementPerformance perf : statementData.performances_temp()) {
+			Play play = perf.play();
 
-            // print line for this order
-            result += String.format("  %s: %s (%s seats)\n", play.name(), usd(amountFor(perf, play)), perf.audience());
-        }
+			// print line for this order
+			result += String.format("  %s: %s (%s seats)\n", play.name(), usd(amountFor(perf, play)), perf.audience());
+		}
 
-        result += String.format("Amount owed is %s\n", usd(totalAmount(statementData.performances(), statementData.plays())));
-        result += String.format("You earned %s credits\n", totalVolumeCredits(statementData.performances(), statementData.plays()));
-        return result;
-    }
+		result += String.format("Amount owed is %s\n", usd(totalAmount(statementData.performances_temp(), statementData.plays())));
+		result += String.format("You earned %s credits\n", totalVolumeCredits(statementData.performances_temp(), statementData.plays()));
+		return result;
+	}
 
-    private int totalAmount(List<Performance> performances, Map<String, Play> plays) {
-        int result = 0;
-        for (Performance perf : performances) {
-            Play play = plays.get(perf.playID());
-            int thisAmount = amountFor(perf, play);
+	private int totalAmount(List<StatementData.StatementPerformance> performances, Map<String, Play> plays) {
+		int result = 0;
+		for (StatementData.StatementPerformance perf : performances) {
+			Play play = perf.play();
+			int thisAmount = amountFor(perf, play);
 
-            result += thisAmount;
-        }
-        return result;
-    }
+			result += thisAmount;
+		}
+		return result;
+	}
 
-    private int totalVolumeCredits(List<Performance> performances, Map<String, Play> plays) {
-        int result = 0;
-        for (Performance perf : performances) {
-            Play play = plays.get(perf.playID());
-            result += volumeCreditsFor(perf, play);
-        }
-        return result;
-    }
+	private int totalVolumeCredits(List<StatementData.StatementPerformance> performances, Map<String, Play> plays) {
+		int result = 0;
+		for (StatementData.StatementPerformance perf : performances) {
+			Play play = perf.play();
+			result += volumeCreditsFor(perf, play);
+		}
+		return result;
+	}
 
-    private static String usd(int thisAmount) {
-        return NumberFormat.getCurrencyInstance(Locale.US).format(thisAmount / 100);
-    }
+	private static String usd(int thisAmount) {
+		return NumberFormat.getCurrencyInstance(Locale.US).format(thisAmount / 100);
+	}
 
-    private static int volumeCreditsFor(Performance perf, Play play) {
-        int result = 0;
-        // add volume credits
-        result += Math.max(perf.audience() - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy".equals(play.type())) result += perf.audience() / 5;
-        return result;
-    }
+	private static int volumeCreditsFor(StatementData.StatementPerformance perf, Play play) {
+		int result = 0;
+		// add volume credits
+		result += Math.max(perf.audience() - 30, 0);
+		// add extra credit for every ten comedy attendees
+		if ("comedy".equals(play.type())) {
+			result += perf.audience() / 5;
+		}
+		return result;
+	}
 
-    private int amountFor(Performance perf, Play play) {
-        int result;
-        switch (play.type()) {
-            case "tragedy":
-                result = 40000;
-                if (perf.audience() > 30) {
-                    result += 1000 * (perf.audience() - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (perf.audience() > 20) {
-                    result += 10000 + 500 * (perf.audience() - 20);
-                }
-                result += 300 * perf.audience();
-                break;
-            default:
-                throw new Error("unknown type: ${play.type}");
-        }
-        return result;
-    }
+	private int amountFor(StatementData.StatementPerformance perf, Play play) {
+		int result;
+		switch (play.type()) {
+			case "tragedy":
+				result = 40000;
+				if (perf.audience() > 30) {
+					result += 1000 * (perf.audience() - 30);
+				}
+				break;
+			case "comedy":
+				result = 30000;
+				if (perf.audience() > 20) {
+					result += 10000 + 500 * (perf.audience() - 20);
+				}
+				result += 300 * perf.audience();
+				break;
+			default:
+				throw new Error("unknown type: ${play.type}");
+		}
+		return result;
+	}
 
-    private static class StatementData {
-        private final List<Performance> performances;
-        private final Map<String, Play> plays;
-        private final String customer;
+	private static class StatementData {
+		private final List<Performance> performances;
+		private final Map<String, Play> plays;
+		private final String customer;
+		private final List<StatementPerformance> performances_temp;
 
-        public StatementData(Invoice invoice, Map<String, Play> plays) {
-            this.customer = invoice.customer();
-            this.performances = Collections.unmodifiableList(invoice.performances());
-            this.plays = plays;
-        }
+		public StatementData(Invoice invoice, Map<String, Play> plays) {
+			this.customer = invoice.customer();
+			this.performances_temp = invoice.performances().stream()
+											.map(performance -> new StatementPerformance(performance, plays.get(
+													performance.playID())))
+											.toList();
+			this.performances = Collections.unmodifiableList(invoice.performances());
+			this.plays = plays;
+		}
 
-        public List<Performance> performances() {
-            return performances;
-        }
+		public List<Performance> performances() {
+			return performances;
+		}
 
-        public Map<String, Play> plays() {
-            return plays;
-        }
+		public Map<String, Play> plays() {
+			return plays;
+		}
 
-        public String customer() {
-            return customer;
-        }
+		public String customer() {
+			return customer;
+		}
 
-    }
+		public List<StatementPerformance> performances_temp() {
+			return performances_temp;
+		}
+
+		static class StatementPerformance {
+			private final int audience;
+			private final Play play;
+
+			public StatementPerformance(Performance performance, Play play) {
+				this.audience = performance.audience();
+				this.play = play;
+			}
+
+			public int audience() {
+				return audience;
+			}
+
+			public Play play() {
+				return play;
+			}
+		}
+	}
 }
