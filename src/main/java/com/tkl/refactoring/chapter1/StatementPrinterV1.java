b73e68c26443a4ee9a8e445401ec764acf6df240
krseo71 <tjrudfhr123@naver.com>
2025-04-21 21:41:36 +0900

    [FIX] playFor 메서드 수정, 변수명 변경 [REFACTOR] usd 메서드 변경

--- src/main/java/com/tkl/refactoring/chapter1/StatementPrinterV1.java
+++ src/main/java/com/tkl/refactoring/chapter1/StatementPrinterV1.java
@@ -1,7 +1,6 @@
 package com.tkl.refactoring.chapter1;
 
 import java.text.NumberFormat;
-import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
@@ -9,66 +8,69 @@ public class StatementPrinterV1 {
 
     public String print(Invoice invoice, Map<String, Play> plays) {
         String result = String.format("Statement for %s\n", invoice.customer());
-        NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
-        for (Performance perf : invoice.performances()) {
-            result += String.format("  %s: %s (%s seats)\n", playFor(perf).name(), frmt.format(amountFor(perf) / 100), perf.audience());
+        for (Performance performance : invoice.performances()) {
+            result += String.format("  %s: %s (%s seats)\n", playFor(performance, plays).name(), usd(amountFor(performance, plays)), performance.audience());
         }
 
-        result += String.format("Amount owed is %s\n", frmt.format(totalAmount(invoice) / 100));
-        result += String.format("You earned %s credits\n", totalVolumeCredits(invoice));
+        result += String.format("Amount owed is %s\n", usd(totalAmount(invoice, plays)));
+        result += String.format("You earned %s credits\n", totalVolumeCredits(invoice, plays));
         return result;
     }
 
-    private static int totalAmount(Invoice invoice) {
+    private String usd(int amount) {
+        return NumberFormat.getCurrencyInstance(Locale.US).format(amount / 100);
+    }
+
+    private int totalAmount(Invoice invoice, Map<String, Play> plays) {
         int result = 0;
-        for (Performance perf : invoice.performances()) {
-            result += amountFor(perf);
+        for (Performance performance : invoice.performances()) {
+            result += amountFor(performance, plays);
         }
         return result;
     }
 
-    private static int totalVolumeCredits(Invoice invoice) {
+    private int totalVolumeCredits(Invoice invoice, Map<String, Play> plays) {
         int result = 0;
-        for (Performance perf : invoice.performances()) {
-            result += volumeCreditsFor(perf);
+        for (Performance performance : invoice.performances()) {
+            result += volumeCreditsFor(performance, plays);
         }
         return result;
     }
 
-    private static int volumeCreditsFor(Performance aPerformance) {
+    private int volumeCreditsFor(Performance performance, Map<String, Play> plays) {
         int result = 0;
         // add volume credits
-        result += Math.max(aPerformance.audience() - 30, 0);
+        result += Math.max(performance.audience() - 30, 0);
         // add extra credit for every ten comedy attendees
-        if ("comedy".equals(playFor(aPerformance).type())) result += aPerformance.audience() / 5;
+        if ("comedy".equals(playFor(performance, plays).type())) result += performance.audience() / 5;
         return result;
     }
 
-    private static Play playFor(Performance perf) {
-        return ((Map<String, Play>) new HashMap<String, Play>()).get(perf.playID());
+    private Play playFor(Performance performance, Map<String, Play> plays) {
+        return plays.get(performance.playID());
     }
 
     //  1. switch문 함수 추출하기
-    private static int amountFor(Performance aPerformance) {
+    private int amountFor(Performance performance, Map<String, Play> plays) {
         // 2. 변수의 이름을 더 명확하게 변경 thisAmount -> result
         int result;
-        switch (playFor(aPerformance).type()) {
+        switch (playFor(performance, plays).type()) {
             case "tragedy":
                 result = 40000;
-                if (aPerformance.audience() > 30) {
-                    result += 1000 * (aPerformance.audience() - 30);
+                if (performance.audience() > 30) {
+                    result += 1000 * (performance.audience() - 30);
                 }
                 break;
             case "comedy":
                 result = 30000;
-                if (aPerformance.audience() > 20) {
-                    result += 10000 + 500 * (aPerformance.audience() - 20);
+                if (performance.audience() > 20) {
+                    result += 10000 + 500 * (performance.audience() - 20);
                 }
-                result += 300 * aPerformance.audience();
+                result += 300 * performance.audience();
                 break;
             default:
-                throw new Error("unknown type: ${playFor(aPerformance).type()}");
+                throw new Error("unknown type: ${playFor(performance).type()}");
         }
         return result;
     }
