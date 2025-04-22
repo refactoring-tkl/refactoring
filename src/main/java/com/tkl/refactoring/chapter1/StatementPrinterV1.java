a52d690f143d1e41e8a4a98e58aee2b392840a25
krseo71 <tjrudfhr123@naver.com>
2025-04-22 20:57:59 +0900

    [REFACTOR] 1.playFor() 메서드 중간 데이터를 사용하도록 변경 2.amountFor, totalAmount, volumeCreditsFor, totalVolumeCredits도 중간 데이터를 사용하도록 변경 3. plays 변수 제거

--- src/main/java/com/tkl/refactoring/chapter1/StatementPrinterV1.java
+++ src/main/java/com/tkl/refactoring/chapter1/StatementPrinterV1.java
@@ -14,16 +14,64 @@ public class StatementPrinterV1 {
     static class StatementData {
         private String customer;
         private List<Performance> performances;
+        private Map<String, Play> plays;
 
-        public StatementData(Invoice invoice) {
+        public StatementData(Invoice invoice, Map<String, Play> plays) {
             this.customer = invoice.customer();
             this.performances = invoice.performances();
+            this.plays = plays;
         }
 
+        public Play playFor(Performance performance) {
+            return plays.get(performance.playID());
+        }
+        private int amountFor(Performance performance) {
+            int result;
+            switch (playFor(performance).type()) {
+                case "tragedy":
+                    result = 40000;
+                    if (performance.audience() > 30) {
+                        result += 1000 * (performance.audience() - 30);
+                    }
+                    break;
+                case "comedy":
+                    result = 30000;
+                    if (performance.audience() > 20) {
+                        result += 10000 + 500 * (performance.audience() - 20);
+                    }
+                    result += 300 * performance.audience();
+                    break;
+                default:
+                    throw new Error("unknown type: ${playFor(performance).type()}");
+            }
+            return result;
+        }
+        private int totalAmount(StatementData statementData) {
+            int result = 0;
+            for (Performance performance : statementData.getPerformances()) {
+                result += amountFor(performance);
+            }
+            return result;
+        }
+        private int volumeCreditsFor(Performance performance) {
+            int result = 0;
+            // add volume credits
+            result += Math.max(performance.audience() - 30, 0);
+            // add extra credit for every ten comedy attendees
+            if ("comedy".equals(playFor(performance).type())) result += performance.audience() / 5;
+            return result;
+        }
+        private int totalVolumeCredits(StatementData statementData) {
+            int result = 0;
+            for (Performance performance : statementData.getPerformances()) {
+                result += volumeCreditsFor(performance);
+            }
+            return result;
+        }
     }
 
     public String print(Invoice invoice, Map<String, Play> plays) {
-        StatementData statementData = new StatementPrinterV1.StatementData(invoice);
+        StatementData statementData = new StatementPrinterV1.StatementData(invoice, plays);
         return renderPlainText(statementData, plays);
     }
 
@@ -31,69 +79,16 @@ public class StatementPrinterV1 {
         String result = String.format("Statement for %s\n", statementData.getCustomer());
 
         for (Performance performance : statementData.getPerformances()) {
-            result += String.format("  %s: %s (%s seats)\n", playFor(performance, plays).name(), usd(amountFor(performance, plays)), performance.audience());
+            result += String.format("  %s: %s (%s seats)\n", statementData.playFor(performance).name(), usd(statementData.amountFor(performance)), performance.audience());
         }
 
-        result += String.format("Amount owed is %s\n", usd(totalAmount(statementData, plays)));
-        result += String.format("You earned %s credits\n", totalVolumeCredits(statementData, plays));
+        result += String.format("Amount owed is %s\n", usd(statementData.totalAmount(statementData)));
+        result += String.format("You earned %s credits\n", statementData.totalVolumeCredits(statementData));
         return result;
     }
 
     private String usd(int amount) {
         return NumberFormat.getCurrencyInstance(Locale.US).format(amount / 100);
-    }
-
-    private int totalAmount(StatementData statementData, Map<String, Play> plays) {
-        int result = 0;
-        for (Performance performance : statementData.getPerformances()) {
-            result += amountFor(performance, plays);
-        }
-        return result;
-    }
-
-    private int totalVolumeCredits(StatementData statementData, Map<String, Play> plays) {
-        int result = 0;
-        for (Performance performance : statementData.getPerformances()) {
-            result += volumeCreditsFor(performance, plays);
-        }
-        return result;
-    }
-
-    private int volumeCreditsFor(Performance performance, Map<String, Play> plays) {
-        int result = 0;
-        // add volume credits
-        result += Math.max(performance.audience() - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy".equals(playFor(performance, plays).type())) result += performance.audience() / 5;
-        return result;
-    }
-
-    private Play playFor(Performance performance, Map<String, Play> plays) {
-        return plays.get(performance.playID());
-    }
-
-    //  1. switch문 함수 추출하기
-    private int amountFor(Performance performance, Map<String, Play> plays) {
-        // 2. 변수의 이름을 더 명확하게 변경 thisAmount -> result
-        int result;
-        switch (playFor(performance, plays).type()) {
-            case "tragedy":
-                result = 40000;
-                if (performance.audience() > 30) {
-                    result += 1000 * (performance.audience() - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (performance.audience() > 20) {
-                    result += 10000 + 500 * (performance.audience() - 20);
-                }
-                result += 300 * performance.audience();
-                break;
-            default:
-                throw new Error("unknown type: ${playFor(performance).type()}");
-        }
-        return result;
     }
 
 }
