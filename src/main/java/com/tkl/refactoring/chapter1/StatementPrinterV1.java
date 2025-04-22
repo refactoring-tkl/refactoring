dfb947816658c71a89bdd5f03ee2b73bb49be284
krseo71 <tjrudfhr123@naver.com>
2025-04-22 21:37:48 +0900

    [REFACTOR] PerformanceCalculator amount 함수 생성

--- src/main/java/com/tkl/refactoring/chapter1/StatementPrinterV1.java
+++ src/main/java/com/tkl/refactoring/chapter1/StatementPrinterV1.java
@@ -9,9 +9,37 @@ import java.util.Locale;
 import java.util.Map;
 
 public class StatementPrinterV1 {
+    @Getter
     static class PerformanceCalculator {
         private Performance performance;
         private Play play;
+
+        public PerformanceCalculator(Performance performance, Play play) {
+            this.performance = performance;
+            this.play = play;
+        }
+
+        private int amount() {
+            int result;
+            switch (play.type()) {
+                case "tragedy":
+                    result = 40000;
+                    if (performance.audience() > 30) {
+                        result += 1000 * (performance.audience() - 30);
+                    }
+                    break;
+                case "comedy":
+                    result = 30000;
+                    if (performance.audience() > 20) {
+                        result += 10000 + 500 * (performance.audience() - 20);
+                    }
+                    result += 300 * performance.audience();
+                    break;
+                default:
+                    throw new Error("unknown type: ${playFor(performance).type()}");
+            }
+            return result;
+        }
     }
 
     @Getter
