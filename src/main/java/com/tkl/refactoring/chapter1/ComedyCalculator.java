757cd0494990cc8d57643298ce5e290c0b333e96
eunkong <dmsrud7280@naver.com>
2025-04-23 01:49:53 +0900

    [REFACTOR] 조건부 로직을 다형성으로 바꾸기 (PerformanceCalculator 추상 클래스로 변환, ComedyCalculator와 TragedyCalculator 추상 메서드 구현)

--- src/main/java/com/tkl/refactoring/chapter1/ComedyCalculator.java
+++ src/main/java/com/tkl/refactoring/chapter1/ComedyCalculator.java
@@ -4,4 +4,14 @@ public class ComedyCalculator extends PerformanceCalculator {
     public ComedyCalculator(Performance performance, Play play) {
         super(performance, play);
     }
+
+    @Override
+    public int getAmounts() {
+        int amounts = 30000;
+        if (this.performance.audience() > 20) {
+            amounts += 10000 + 500 * (this.performance.audience() - 20);
+        }
+        amounts += 300 * this.performance.audience();
+        return amounts;
+    }
 }
--- src/main/java/com/tkl/refactoring/chapter1/TragedyCalculator.java
+++ src/main/java/com/tkl/refactoring/chapter1/TragedyCalculator.java
@@ -4,4 +4,13 @@ public class TragedyCalculator extends PerformanceCalculator {
     public TragedyCalculator(Performance performance, Play play) {
         super(performance, play);
     }
+
+    @Override
+    public int getAmounts() {
+        int amounts = 40000;
+        if (this.performance.audience() > 30) {
+            amounts += 1000 * (this.performance.audience() - 30);
+        };
+        return amounts;
+    }
 }
