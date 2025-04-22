608c317fdfb90271bc839e70429b312fa7e55d41
eunkong <dmsrud7280@naver.com>
2025-04-23 01:37:30 +0900

    [REFACTOR] 메서드 사용 수정 (직접 호출 -> 계산기 호출), 계산기 호출로 인한 불필요한 메서드(getVolumeCredits, getAmounts) 삭제

--- src/main/java/com/tkl/refactoring/chapter1/StatementDataCalculator.java
+++ src/main/java/com/tkl/refactoring/chapter1/StatementDataCalculator.java
@@ -1,47 +1,44 @@
 package com.tkl.refactoring.chapter1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class StatementDataCalculator {
     public StatementData createStatementData(Invoice invoice, Map<String, Play> plays) {
         return new StatementData(invoice.customer(),
-                                                        enrichPerformances(invoice.performances(), plays),
-                                                        getTotalAmounts(enrichPerformances(invoice.performances(), plays)),
-                                                        getTotalVolumeCredits(enrichPerformances(invoice.performances(), plays)));
+                enrichPerformances(invoice.performances(), plays),
+                getTotalAmounts(enrichPerformances(invoice.performances(), plays)),
+                getTotalVolumeCredits(enrichPerformances(invoice.performances(), plays)));
     }
 
     private List<EnrichedPerformance> enrichPerformances(List<Performance> performances, Map<String, Play> plays) {
         return performances.stream()
-                            .map(p -> new EnrichedPerformance(new Performance(p.playID(), p.audience()),
-                                                                findByPerformancePlayId(plays, p),
-                                                                getAmounts(p, findByPerformancePlayId(plays, p)),
-                                                                getVolumeCredits(p, findByPerformancePlayId(plays, p))))
-                            .toList();
+                .map(p -> {
+                    PerformanceCalculator calculator = new PerformanceCalculator(p, findByPerformancePlayId(plays, p));
+                    return new EnrichedPerformance(new Performance(p.playID(), p.audience()),
+                            findByPerformancePlayId(plays, p),
+                            calculator.getAmounts(),
+                            calculator.getVolumeCredits());
+                })
+                .toList();
     }
 
 
     private int getTotalAmounts(List<EnrichedPerformance> enrichedPerformances) {
         return enrichedPerformances.stream()
-                                    .mapToInt(EnrichedPerformance::amount)
-                                    .sum();
+                .mapToInt(EnrichedPerformance::amount)
+                .sum();
     }
 
     private int getTotalVolumeCredits(List<EnrichedPerformance> enrichedPerformances) {
         return enrichedPerformances.stream()
-                                    .mapToInt(EnrichedPerformance::volumeCredits)
-                                    .sum();
-    }
-
-    private int getVolumeCredits(Performance perf, Play play) {
-        return new PerformanceCalculator(perf, play).getVolumeCredits();
+                .mapToInt(EnrichedPerformance::volumeCredits)
+                .sum();
     }
 
     private Play findByPerformancePlayId(Map<String, Play> plays, Performance perf) {
         return plays.get(perf.playID());
     }
-
-    private int getAmounts(Performance perf, Play play) {
-        return new PerformanceCalculator(perf, play).getAmounts();
-    }
 }
+
