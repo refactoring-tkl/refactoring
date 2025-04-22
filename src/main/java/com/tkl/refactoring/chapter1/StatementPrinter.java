4bed53d91044536edd67a5192c9c0cf738a68807
eunkong <dmsrud7280@naver.com>
2025-04-23 01:06:05 +0900

    [REFACTOR] HTML 버전 추가

--- src/main/java/com/tkl/refactoring/chapter1/StatementPrinter.java
+++ src/main/java/com/tkl/refactoring/chapter1/StatementPrinter.java
@@ -24,6 +24,27 @@ public class StatementPrinter {
         return result;
     }
 
+    public String printHtml(Invoice invoice, Map<String, Play> plays) {
+        return renderHtml(new StatementDataCalculator().createStatementData(invoice, plays));
+    }
+
+    private String renderHtml(StatementData data) {
+        String result = String.format("<h1>Statement for:  %s<h1>\n", data.customer());
+        result += "<table>\n";
+        result += "<tr><th>Play</th><th>Seats</th><th>Amount</th></tr>";
+        for (EnrichedPerformance enrichedPerf : data.enrichedPerformances()) {
+            // print line for this order
+            result += String.format("   <tr><td>%s</td><td>%s</td><td>%s</td></tr>\n",
+                                        enrichedPerf.play().name(),
+                                        usd().format(enrichedPerf.amount() / 100),
+                                        enrichedPerf.performance().audience());
+        }
+        result += "</table>\n";
+        result += String.format("<p>Amount owed is <em>%s</em></p>\n", usd().format(data.totalAmounts() / 100));
+        result += String.format("<p>You earned <em>%s</em> credits<p>\n", data.totalVolumeCredits());
+        return result;
+    }
+
     private NumberFormat usd() {
         return NumberFormat.getCurrencyInstance(Locale.US);
     }
