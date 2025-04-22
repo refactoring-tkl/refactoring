994bb92a43826c9132b15f653493001aa2616433
eunkong <dmsrud7280@naver.com>
2025-04-23 01:20:53 +0900

    [REFACTOR] 공연료 계산기(PerformanceCalculator) 만들기 및 메서드(getAmounts) 옮기기

---
+++ src/main/java/com/tkl/refactoring/chapter1/PerformanceCalculator.java
@@ -0,0 +1,33 @@
+package com.tkl.refactoring.chapter1;
+
+public class PerformanceCalculator {
+    private final Performance performance;
+    private final Play play;
+
+    public PerformanceCalculator(Performance performance, Play play) {
+        this.performance = performance;
+        this.play = play;
+    }
+
+    public int getAmounts() {
+        int amounts = 0;
+        switch (this.play.type()) {
+            case "tragedy":
+                amounts = 40000;
+                if (this.performance.audience() > 30) {
+                    amounts += 1000 * (this.performance.audience() - 30);
+                }
+                break;
+            case "comedy":
+                amounts = 30000;
+                if (this.performance.audience() > 20) {
+                    amounts += 10000 + 500 * (this.performance.audience() - 20);
+                }
+                amounts += 300 * this.performance.audience();
+                break;
+            default:
+                throw new Error("unknown type: ${play.type}");
+        }
+        return amounts;
+    }
+}
