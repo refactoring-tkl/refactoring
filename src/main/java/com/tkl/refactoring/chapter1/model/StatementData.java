c44db44855f2f28c8a15e5bcfd4f4a55968a6e31
krseo71 <tjrudfhr123@naver.com>
2025-04-29 20:54:05 +0900

    [REFACTOR] 불필요 로직 제거, Invoice 변수로 통합, PerformanceCalculatorFactory 의존관계 주입

--- src/main/java/com/tkl/refactoring/chapter1/model/StatementData.java
+++ src/main/java/com/tkl/refactoring/chapter1/model/StatementData.java
@@ -12,33 +12,32 @@ import java.util.Map;
 
 @Getter
 public class StatementData {
-    private String customer;
-    private List<Performance> performances;
+    private Invoice invoice;
     private Map<String, Play> plays;
+    private PerformanceCalculatorFactory performanceCalculatorFactory;
 
     public StatementData(Invoice invoice, Map<String, Play> plays) {
-        this.customer = invoice.customer();
-        this.performances = invoice.performances();
+        this.invoice = invoice;
         this.plays = plays;
+        this.performanceCalculatorFactory = new PerformanceCalculatorFactory();
     }
 
     public Play playFor(Performance performance) {
         return plays.get(performance.playID());
     }
     public int amountFor(Performance performance) {
-        return new PerformanceCalculatorFactory().createPerformanceCalculator(performance, playFor(performance)).amount();
+        return performanceCalculatorFactory.createPerformanceCalculator(performance, playFor(performance)).amount();
     }
     public int volumeCreditsFor(Performance performance) {
-        return new PerformanceCalculatorFactory().createPerformanceCalculator(performance, playFor(performance)).volumeCredits();
-//            return new PerformanceCalculator(performance, playFor(performance)).volumeCredits();
+        return performanceCalculatorFactory.createPerformanceCalculator(performance, playFor(performance)).volumeCredits();
     }
     public int totalAmount(StatementData statementData) {
-        return statementData.performances.stream()
+        return statementData.invoice.performances().stream()
                 .mapToInt(this::amountFor)
                 .sum();
     }
     public int totalVolumeCredits(StatementData statementData) {
-        return statementData.getPerformances().stream()
+        return statementData.invoice.performances().stream()
                 .mapToInt(this::volumeCreditsFor)
                 .sum();
     }
