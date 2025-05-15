37910c4205b2cc71d6c78bba334244e01af461dc
eunkyung.shin <eunkyun.shin@nhnlink.co.kr>
2025-05-15 22:37:32 +0900

    [ADD] 3.1 기이한 이름 예시

---
+++ src/main/java/com/tkl/refactoring/chapter3/MysteriousName.java
@@ -0,0 +1,18 @@
+package main.java.com.tkl.refactoring.chapter3;
+
+public class MysteriousName {
+	// pcweb > FacilityLoginController
+	private static final String SSO_DELIMETER = "!@#";
+
+	private String makeSSOHashData(String partnerNo, String memberNo, String tid, String timestamp) {
+		String combined = partnerNo + SSO_DELIMETER + memberNo + SSO_DELIMETER + tid + SSO_DELIMETER + timestamp;
+		String md5 = md5(combined);
+		return md5;
+	}
+
+	private static String md5(String input) {
+		// execute md5 hashing...
+		String md5Hash = input;
+		return md5Hash;
+	}
+}
