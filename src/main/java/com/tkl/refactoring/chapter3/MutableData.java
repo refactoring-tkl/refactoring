ea07cb27dbca16a9e0681f7d376407a3a268db2a
eunkyung.shin <eunkyun.shin@nhnlink.co.kr>
2025-05-21 23:35:53 +0900

    [ADD] 3.6 가변 데이터 예시

---
+++ src/main/java/com/tkl/refactoring/chapter3/MutableData.java
@@ -0,0 +1,9 @@
+package com.tkl.refactoring.chapter3;
+
+// 3.6 가변 데이터
+public class MutableData {
+	// BsArtApiClient.timeout
+	// BcArtApiClient.timeout
+	// LotteApiClient.timeout
+	// LgArtApiClient.timeout
+}
