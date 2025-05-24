9704fdcf0fc2c6ccc83e334ceab8a060d72bf91e
eunkong <dmsrud7280@naver.com>
2025-05-24 23:54:44 +0900

    [ADD] 핸드폰 요금 계산 초기 코드 예시
    (cherry picked from commit 377f536991b69af3ac537d3465bb5b9e27e2a208)

---
+++ src/main/java/com/tkl/refactoring/object/phone/NightlyDiscountPhone.java
@@ -0,0 +1,28 @@
+package com.tkl.refactoring.object.phone;
+
+import com.tkl.refactoring.object.common.Call;
+import com.tkl.refactoring.object.common.Money;
+
+import java.time.Duration;
+
+public class NightlyDiscountPhone extends AbstractPhone {
+    private static final int LATE_NIGHT_HOUR = 22;
+
+    private final Money nightlyAmount;
+    private final Money regularAmount;
+    private final Duration seconds;
+
+    public NightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds) {
+        this.nightlyAmount = nightlyAmount;
+        this.regularAmount = regularAmount;
+        this.seconds = seconds;
+    }
+
+    @Override
+    protected Money calculateFee(Call call) {
+        if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
+            return nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds());
+        }
+        return regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds());
+    }
+}
