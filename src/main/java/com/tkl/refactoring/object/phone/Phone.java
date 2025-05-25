e46eaff5d1c16d896fce75b0370c1c7c6eed8927
eunkong <dmsrud7280@naver.com>
2025-05-25 16:28:37 +0900

    [REFACTOR] 정책 사용 방식 변경 (상속 -> 합성)

--- src/main/java/com/tkl/refactoring/object/phone/Phone.java
+++ src/main/java/com/tkl/refactoring/object/phone/Phone.java
@@ -2,20 +2,48 @@ package com.tkl.refactoring.object.phone;
 
 import com.tkl.refactoring.object.common.Call;
 import com.tkl.refactoring.object.common.Money;
+import com.tkl.refactoring.object.policy.addition.RateDiscountablePolicy;
+import com.tkl.refactoring.object.policy.addition.TaxablePolicy;
+import com.tkl.refactoring.object.policy.basic.NightlyDiscountPolicy;
+import com.tkl.refactoring.object.policy.RatePolicy;
+import com.tkl.refactoring.object.policy.basic.RegularPolicy;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class Phone extends AbstractPhone {
-    private final Money amount;
-    private final Duration seconds;
+public class Phone {
+    private RatePolicy ratePolicy;
+    private List<Call> calls = new ArrayList<>();
 
-    public Phone(Money amount, Duration seconds) {
-        this.amount = amount;
-        this.seconds = seconds;
+    public Phone(RatePolicy ratePolicy) {
+        this.ratePolicy = ratePolicy;
     }
 
-    @Override
-    protected Money calculateFee(Call call) {
-        return amount.times(call.getDuration().getSeconds() / seconds.getSeconds());
+    public void call(Call call) { calls.add(call); }
+    public List<Call> getCalls() { return Collections.unmodifiableList(calls); }
+
+    public Money calculateFee() {
+        return ratePolicy.calculateFee(this);
+    }
+
+    public static void main(String[] args) {
+        Phone taxableRegularPhone = new Phone(
+                                        new TaxablePolicy(0.05,
+                                            new RegularPolicy(Money.wons(10), Duration.ofSeconds(10))));
+        Phone taxableRateDiscountableRegularPhone = new Phone(
+                                                        new TaxablePolicy(0.05,
+                                                            new RateDiscountablePolicy(Money.wons(1000),
+                                                                new RegularPolicy(Money.wons(10), Duration.ofSeconds(10)))));
+
+        Phone rateDiscountableTaxableRegularPhone = new Phone(
+                                                        new RateDiscountablePolicy(Money.wons(1000),
+                                                            new TaxablePolicy(0.05,
+                                                                new RegularPolicy(Money.wons(10), Duration.ofSeconds(10)))));
+        Phone rateDiscountableTaxableNightlyDiscountPhone = new Phone(
+                                                                new RateDiscountablePolicy(Money.wons(1000),
+                                                                    new TaxablePolicy(0.05,
+                                                                        new NightlyDiscountPolicy(Money.wons(5), Money.wons(10), Duration.ofSeconds(10)))));
     }
 }
