becc311e83948b379e29cc375bc5af1856a1a177
eunkyung.shin <eunkyun.shin@nhnlink.co.kr>
2025-05-27 18:13:48 +0900

    [ADD] 3.11 기본형 집착 예시

---
+++ src/main/java/com/tkl/refactoring/chapter3/PrimitiveObsession.java
@@ -0,0 +1,16 @@
+package com.tkl.refactoring.chapter3;
+
+public class PrimitiveObsession {
+	public String setProductLogicalPlanPCache(int productId) {
+		if (productId == -1){
+			//
+			// return ...
+		}
+
+		if (productId == -2){
+			// ...
+			// return ...
+		}
+		return null;
+	}
+}
