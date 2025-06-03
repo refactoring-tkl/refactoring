5e8611446b541b9158a89f7533c1f73ac9795831
eunkong <dmsrud7280@naver.com>
2025-06-03 12:02:51 +0900

    [REFACTOR] 요금 계산 설계 수정으로 인한 기존 요금제 (일반 요금제, 심야 할인 요금제) 삭제, 삭제 및 설계 변경으로 인한 사용 코드 수정

--- src/main/java/com/tkl/refactoring/object/phone/Phone.java
+++ src/main/java/com/tkl/refactoring/object/phone/Phone.java
@@ -2,13 +2,20 @@ package com.tkl.refactoring.object.phone;
 
 import com.tkl.refactoring.object.common.Call;
 import com.tkl.refactoring.object.common.Money;
+import com.tkl.refactoring.object.fee.FeePerDuration;
+import com.tkl.refactoring.object.fee.FeeRule;
+import com.tkl.refactoring.object.fee.condition.DayOfWeekFeeCondition;
+import com.tkl.refactoring.object.fee.condition.DurationFeeCondition;
+import com.tkl.refactoring.object.fee.condition.FixedFeeCondition;
+import com.tkl.refactoring.object.fee.condition.TimeOfDayFeeCondition;
 import com.tkl.refactoring.object.policy.addition.RateDiscountablePolicy;
 import com.tkl.refactoring.object.policy.addition.TaxablePolicy;
-import com.tkl.refactoring.object.policy.basic.NightlyDiscountPolicy;
+import com.tkl.refactoring.object.policy.basic.BasicRatePolicy;
 import com.tkl.refactoring.object.policy.RatePolicy;
-import com.tkl.refactoring.object.policy.basic.RegularPolicy;
 
+import java.time.DayOfWeek;
 import java.time.Duration;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -29,21 +36,48 @@ public class Phone {
     }
 
     public static void main(String[] args) {
-        Phone taxableRegularPhone = new Phone(
-                                        new TaxablePolicy(0.05,
-                                            new RegularPolicy(Money.wons(10), Duration.ofSeconds(10))));
-        Phone taxableRateDiscountableRegularPhone = new Phone(
-                                                        new TaxablePolicy(0.05,
-                                                            new RateDiscountablePolicy(Money.wons(1000),
-                                                                new RegularPolicy(Money.wons(10), Duration.ofSeconds(10)))));
+        Phone taxablFixedFeePhone
+            = new Phone(
+                new TaxablePolicy(0.05,
+                    new BasicRatePolicy(
+                        new FeeRule(
+                            new FixedFeeCondition(),
+                                new FeePerDuration(Money.wons(10), Duration.ofSeconds(10))))));
 
-        Phone rateDiscountableTaxableRegularPhone = new Phone(
-                                                        new RateDiscountablePolicy(Money.wons(1000),
-                                                            new TaxablePolicy(0.05,
-                                                                new RegularPolicy(Money.wons(10), Duration.ofSeconds(10)))));
-        Phone rateDiscountableTaxableNightlyDiscountPhone = new Phone(
-                                                                new RateDiscountablePolicy(Money.wons(1000),
-                                                                    new TaxablePolicy(0.05,
-                                                                        new NightlyDiscountPolicy(Money.wons(5), Money.wons(10), Duration.ofSeconds(10)))));
+        Phone taxableRateDiscountableTimeOfDayFeePhone
+            = new Phone(
+                new TaxablePolicy(0.05,
+                    new RateDiscountablePolicy(Money.wons(1000),
+                        new BasicRatePolicy(
+                            new FeeRule(
+                                new TimeOfDayFeeCondition(LocalTime.of(0,0,0), LocalTime.of(18,59,59)),
+                                    new FeePerDuration(Money.wons(18), Duration.ofSeconds(10))),
+                            new FeeRule(
+                                new TimeOfDayFeeCondition(LocalTime.of(19,0,0), LocalTime.of(23, 59,59)),
+                                    new FeePerDuration(Money.wons(15), Duration.ofSeconds(10)))))));
+
+        Phone rateDiscountableTaxableDayOfWeekFeePhone
+            = new Phone(
+                new RateDiscountablePolicy(Money.wons(1000),
+                    new TaxablePolicy(0.05,
+                        new BasicRatePolicy(
+                            new FeeRule(
+                                new DayOfWeekFeeCondition(DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY, DayOfWeek.FRIDAY),
+                                    new FeePerDuration(Money.wons(38), Duration.ofSeconds(10))),
+                            new FeeRule(
+                                new DayOfWeekFeeCondition(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY),
+                                    new FeePerDuration(Money.wons(19), Duration.ofSeconds(10)))))));
+
+        Phone rateDiscountableTaxableNightlyDiscountPhone
+            = new Phone(
+                new RateDiscountablePolicy(Money.wons(1000),
+                    new TaxablePolicy(0.05,
+                        new BasicRatePolicy(
+                            new FeeRule(
+                                new DurationFeeCondition(Duration.ofSeconds(0), Duration.ofMinutes(1)),
+                                    new FeePerDuration(Money.wons(50), Duration.ofSeconds(10))),
+                            new FeeRule(
+                                new DurationFeeCondition(Duration.ofMinutes(1), Duration.ofDays(365)),
+                                    new FeePerDuration(Money.wons(20), Duration.ofSeconds(10)))))));
     }
 }
