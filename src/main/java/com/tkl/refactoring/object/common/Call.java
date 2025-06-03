981180323be2dd773d70e68f961abf1fec751336
eunkong <dmsrud7280@naver.com>
2025-06-03 10:11:24 +0900

    [ADD] 통화 일자 구분을 위한 DateTimeInterval 추가 및 Call 사용 변경

--- src/main/java/com/tkl/refactoring/object/common/Call.java
+++ src/main/java/com/tkl/refactoring/object/common/Call.java
@@ -2,19 +2,22 @@ package com.tkl.refactoring.object.common;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class Call {
-    private final LocalDateTime from;
-    private final LocalDateTime to;
+    private DateTimeInterval interval;
 
     public Call(LocalDateTime from, LocalDateTime to) {
-        this.from = from;
-        this.to = to;
+        this.interval = DateTimeInterval.of(from, to);
     }
 
     public Duration getDuration() {
-        return Duration.between(from, to);
+        return interval.duration();
     }
-    public LocalDateTime getFrom() { return from; }
-    public LocalDateTime getTo() { return to; }
+    public LocalDateTime getFrom() { return interval.getFrom(); }
+    public LocalDateTime getTo() { return interval.getTo(); }
+    public DateTimeInterval getInterval() { return interval; }
+    public List<DateTimeInterval> splitByDay() {
+        return interval.splitByDay();
+    }
 }
