af460279534a61a9decce84acdfb77cc65ac2bc2
eunkyung.shin <eunkyun.shin@nhnlink.co.kr>
2025-06-04 22:51:12 +0900

    [ADD] 3.18 중개자 예시

---
+++ src/main/java/com/tkl/refactoring/chapter3/MiddleMan.java
@@ -0,0 +1,9 @@
+package com.tkl.refactoring.chapter3;
+
+// 3.18 중개자
+public class MiddleMan {
+	// LoginValidator
+ 	// 	private boolean isSecondAuthSkipValid(Member member) {
+	//		return adminSecondAuthLoginService.isSecondAuthSkipValid(member.getMemberNo());
+	//	}
+}
