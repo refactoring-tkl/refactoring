c52cd4aaae0baa5a7dadfd66e66442bae62e79b1
eunkyung.shin <eunkyun.shin@nhnlink.co.kr>
2025-06-25 00:40:56 +0900

    [ADD] 6.6 변수 캡슐화하기

---
+++ src/main/java/com/tkl/refactoring/chapter6/EncapsulateVariable.java
@@ -0,0 +1,7 @@
+package com.tkl.refactoring.chapter6;
+
+// 6.6 변수 캡슐화하기
+public class EncapsulateVariable {
+    // 변경 전: LastLoginDatetimeTasklet.readCount
+    // 변경 후: LastLoginDatetimeTasklet.readCount 삭제 (추후 변수 사용할 것 같으면 private)
+}
